5d426cb79a445566130abb39c16964ea0f3850ed
ganesh <ganeshbarbole9568@gmail.com>
2023-10-27 23:40:12 +0530

    ""

---
+++ Blog_Project/src/Components/Postcard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import databaseService from "../appwrite/DatabaseService";
+import {Link} from 'react-router-dom'
+
+function Postcard({$id,title,feacturedImage}){
+    return(
+      <Link to = {`/Postcard/${$id}`}>
+        <div className="w-full bg-gray-100 rounded-xl p-4">
+            <div className="w-full justify-center mb-4">
+              <img src = {databaseService.getFilePreview(feacturedImage)} alt={title} 
+              className="rounded-lg"/>
+            </div>
+            <h2 className="text-xl font-bold">{title}</h2>
+        </div>
+      </Link>
+    )
+}
