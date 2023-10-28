ca43fa40f032f1d7b23daca94e574901f71ed4de
ganesh <ganeshbarbole9568@gmail.com>
2023-10-28 17:19:25 +0530

    React hook form

--- Blog_Project/src/Components/index.js
+++ Blog_Project/src/Components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./logo";
 import Logout from './Header/Logout'
+import Button from './Header'
+import Input from './Header'
 
 export{Header,
        Footer,
        Container,
        Logo,
-       Logout 
+       Logout ,
+       Button,
+       Input
     }
